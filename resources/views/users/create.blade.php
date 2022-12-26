cf30d0cb32c75be0b984ef0f59e82b0512759d4e
Jose Carlos <josecarlosromeiro1986@gmail.com>
2022-12-26 17:07:01 -0300

    tailwind css

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 @section('title', 'Criar novo Usuário')
 @section('content')
-    <h1>Novo usuário</h1>
+    <h1 class="text-2xl font-semibold leading-tigh py-2">Novo Usuário</h1>
 
     @include('includes.validations-form')
 
     <form action="{{ route('users.store') }}" method="post">
-        @csrf
         @include('users._partials.form')
     </form>
 @endsection
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 @section('title', 'Editar o Usuário')
 @section('content')
-    <h1>Editar o usuário {{ $user->name }}</h1>
+    <h1 class="text-2xl font-semibold leading-tigh py-2">Editar o Usuário {{ $user->name }}</h1>
 
     @include('includes.validations-form')
 
     <form action="{{ route('users.update', $user->id) }}" method="post">
-        @csrf
         @method('PUT')
         @include('users._partials.form')
     </form>
